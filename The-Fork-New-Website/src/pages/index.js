1d1459c6fa21cf29252b81e6a1668445e5c4acab
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2019-10-18 14:59:15 +0200

    Aboutus main
    first push

--- The-Fork-New-Website/src/pages/index.js
+++ The-Fork-New-Website/src/pages/index.js
@@ -7,11 +7,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Page Components
 import Footer from '../components/Footer/Footer'
+import About from '../components/About/About'
 
 // Style
 import './style.css'
 
 export default () => <div>
+    <About />
     <Footer />
-
 </div>
