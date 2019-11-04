10be63295730ecbc92b89d32958f75a492829d29
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2019-11-04 10:24:50 +0100

    title update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'The Fork',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'The fork blockchain busnises',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
